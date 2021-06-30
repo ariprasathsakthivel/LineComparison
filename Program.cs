8581b36ac7739b4e223bf6ab4c56718865c7e69e
ariprasathsakthivel <ariprasath3@gmail.com>
2021-06-30 21:36:24 +0530

    added: UC1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿//Welcome to the program. In this program we're going to compare the lines
+﻿
+using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, y1, x2, y2, lengthOfLine; 
+            Console.WriteLine("Enter the coordinate X1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the coordinate Y1");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the coordinate X2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the coordinate Y2");
+            y2 = Convert.ToInt32(Console.ReadLine());
+            lengthOfLine = (int)Math.Sqrt(Convert.ToDouble((x2 - x1) ^ 2 + (y2 - y1) ^ 2));
+            Console.WriteLine("Length of Line is {0}", lengthOfLine);
+        }
+    }
+}
